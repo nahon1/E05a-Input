c605e680ea0050494e8074ab16020c6914f166e3
nahon1 <60366732+nahon1@users.noreply.github.com>
2020-02-12 17:37:27 -0500

    Practice
    Edits the game file

--- Scripts/Label.gd
+++ Scripts/Label.gd
@@ -6,3 +6,8 @@ func _ready():
 
 func _physics_process(delta):
 	pass
+	get_viewport().get_mouse_position()
+	var mouse_pos = get_viewport().get_mouse_position()
+	print(mouse_pos.x)
+	text = str(mouse_pos.x) + ", " + str(mouse_pos.y)
+	
